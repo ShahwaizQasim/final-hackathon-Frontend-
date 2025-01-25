b9f4cd16024134bb90964b898227fb7bdee6bc33
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2025-01-26 02:25:53 +0500

    useContext ready

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -11,29 +11,29 @@ export default function AuthContextProvider({ children }) {
 
     const [user, setUser] = useState(null);
 
-    // useEffect(() => {
-    //     if (!user) {
-    //         const token = Cookies.get('token');
-    //         if (token) {
-    //             getUser(token);
-    //         } else {
-    //             <Navigate to={'signUp'} />
-    //         }
-    //     }
-    // }, [user])
+    useEffect(() => {
+        if (!user) {
+            const token = Cookies.get('token');
+            if (token) {
+                getUser(token);
+            } else {
+                <Navigate to={'signUp'} />
+            }
+        }
+    }, [user])
 
-    // const getUser = async (token) => {
-    //     try {
-    //         const user = await axios.get(APP_ROUTES.userInfo, {
-    //             headers: {
-    //                 Authorization: `Bearer ${token}`
-    //             }
-    //         })
-    //         setUser(user?.data?.user)
-    //     } catch (error) {
-    //         console.log('error in context=>', error);
-    //     }
-    // }
+    const getUser = async (token) => {
+        try {
+            const user = await axios.get(APP_ROUTES.userInfo, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            setUser(user?.data?.user)
+        } catch (error) {
+            console.log('error in context=>', error);
+        }
+    }
 
     console.log('user in context', user);
 
